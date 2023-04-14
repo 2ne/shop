5893ee0b885108d76a61cb45a11d704c8774cb82
James <jamestoone@gmail.com>
2023-04-14 15:17:05 +0100

    Update day.tsx

--- src/components/calendar-week/day.tsx
+++ src/components/calendar-week/day.tsx
@@ -9,7 +9,7 @@ interface CalendarWeekDayProps {
 const CalendarWeekDay: React.FC<CalendarWeekDayProps> = ({ day }) => {
   return (
     <section className="relative flex-grow w-full lg:w-auto lg:max-w-[25%] shrink-0 lg:shrink">
-      <div className="sticky top-0 z-20 flex items-center py-3 mb-3 lg:mb-0 lg:static">
+      <div className="sticky top-0 z-20 flex items-center py-3 mb-3 lg:mb-0 lg:static backdrop-blur bg-blend-overlay bg-white/90">
         <Button shape="circle" className="!rounded-md bg-white lg:hidden">
           <svg
             width="24"
@@ -28,7 +28,7 @@ const CalendarWeekDay: React.FC<CalendarWeekDayProps> = ({ day }) => {
           </svg>
         </Button>
         <div className="flex-grow text-center lg:text-left">
-          <span className="px-4 py-2 rounded-full bg-white/95 lg:p-0 lg:bg-transparent lg:rounded-none heading">
+          <span className="px-4 py-2 rounded-full lg:p-0 lg:bg-transparent lg:rounded-none heading">
             {day}
           </span>
         </div>
