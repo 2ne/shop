c3b59d9d1476825d1fe174dfbbedf3ac3db37657
James <jamestoone@gmail.com>
2023-04-16 09:34:26 +0100

    Update org.js

--- src/org.js
+++ src/org.js
@@ -1,4 +1,4 @@
-/* export const orgName = "CG Swim School";
+export const orgName = "CG Swim School";
 export const orgLogo = "./src/assets/logo.png";
 
 export const orgColours = {
@@ -11,63 +11,53 @@ export const orgColours = {
 
 export const orgTiles = [
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/6f2e0f06-8900-4dc0-981a-0259989f1871",
+    img: "https://app.joinin.online/services/anonymous/photo/file/6f2e0f06-8900-4dc0-981a-0259989f1871",
     title: "Adult and Child Lessons",
     link: "/AdultChildLessons",
   },
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/788667ca-19eb-4818-88f2-8b895dadcdf2",
+    img: "https://app.joinin.online/services/anonymous/photo/file/788667ca-19eb-4818-88f2-8b895dadcdf2",
     title: "Independent Children's Lessons",
     link: "/AdultChildLessons",
   },
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/53602522-6ebd-4ce5-83a1-4ea35f8f1efe",
+    img: "https://app.joinin.online/services/anonymous/photo/file/53602522-6ebd-4ce5-83a1-4ea35f8f1efe",
     title: "Adult Lessons",
     link: "/AdultChildLessons",
   },
-    {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/58484fa4-ef77-43c0-9c6f-34d6a47be27f",
+  {
+    img: "https://app.joinin.online/services/anonymous/photo/file/58484fa4-ef77-43c0-9c6f-34d6a47be27f",
     title: "Private Lessons",
     link: "/AdultChildLessons",
   },
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/da5cce7f-faba-46c0-b160-e0dad0d5fbb7",
+    img: "https://app.joinin.online/services/anonymous/photo/file/da5cce7f-faba-46c0-b160-e0dad0d5fbb7",
     title: "Adult and Child Lessons",
     link: "/AdultChildLessons",
   },
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/292eec5b-ab54-41be-83ce-e70f00257b41",
+    img: "https://app.joinin.online/services/anonymous/photo/file/292eec5b-ab54-41be-83ce-e70f00257b41",
     title: "Independent Children's Lessons",
     link: "/AdultChildLessons",
   },
-    {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/a3ec16f6-f8ff-40d9-8f47-3d15fa85d932",
+  {
+    img: "https://app.joinin.online/services/anonymous/photo/file/a3ec16f6-f8ff-40d9-8f47-3d15fa85d932",
     title: "Adult Lessons",
     link: "/AdultChildLessons",
   },
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/540d3059-e4e3-4ef5-a516-c24f1bed3c50",
+    img: "https://app.joinin.online/services/anonymous/photo/file/540d3059-e4e3-4ef5-a516-c24f1bed3c50",
     title: "Private Lessons",
     link: "/AdultChildLessons",
   },
   {
-    img:
-      "https://app.joinin.online/services/anonymous/photo/file/06e8f09a-6232-4246-b75d-65739ccaade2",
+    img: "https://app.joinin.online/services/anonymous/photo/file/06e8f09a-6232-4246-b75d-65739ccaade2",
     title: "Crash Courses",
     link: "/AdultChildLessons",
   },
 ];
-*/
 
-export const orgName = "Harlow Gymnastics Club";
+/* export const orgName = "Harlow Gymnastics Club";
 export const orgLogo =
   "https://app.joinin.online/services/anonymous/avatar/d0745f9a-3ee4-4774-8746-56c31918efa5";
 
@@ -95,3 +85,4 @@ export const orgTiles = [
     link: "/AdultChildLessons",
   },
 ];
+ */
