553c8463c3203165ffcd48a28d33184bc5f2f942
James <jamestoone@gmail.com>
2023-04-17 16:19:57 +0100

    new header

--- src/components/calendar-week.tsx
+++ src/components/calendar-week.tsx
@@ -24,7 +24,7 @@ const CalendarWeek: React.FC = () => {
 
   return (
     <>
-      <div className="mb-0.5 lg:pb-4 lg:mb-4 lg:flex lg:items-end lg:border-b lg:border-neutral-200">
+      <div className="mb-0.5 lg:-mt-3 lg:py-3 lg:mb-6 lg:flex lg:items-end lg:border-b lg:border-neutral-200 lg:sticky lg:top-0 lg:z-10 lg:bg-white/95">
         <h2 className="heading-lg">Adult and Child Lessons</h2>
         <div className="max-lg:overflow-x-auto max-lg:z-30 max-lg:bg-white/95 max-lg:fixed max-lg:bottom-0 max-lg:left-0 max-lg:right-0 lg:ml-auto max-lg:py-2.5 max-lg:border-t max-lg:border-black/10">
           <div className="container flex items-center gap-2 lg:gap-2.5 lg:p-0">
