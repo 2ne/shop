7032d3f9b941e966f31587dcd4ddf5c18e6ee73b
James <jamestoone@gmail.com>
2023-04-17 19:23:33 +0100

    componotize events

--- src/components/calendar-week.tsx
+++ src/components/calendar-week.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Select } from "antd";
 import { orgEvents, Event } from "../org";
 import { Link } from "react-router-dom";
+import CalendarEvent from "./calendar-event";
 
 type SelectedValue = string | undefined;
 
@@ -144,127 +145,20 @@ const CalendarWeek: React.FC = () => {
       </div>
       <div className="grid grid-cols-1 gap-3 p-px lg:hidden">
         {orgEvents[activeDay].map((event: Event, index: number) => (
-          <Link
-            key={`${activeDay}-event-${index}`}
-            to={event.link}
-            className="block p-3 text-sm card max-lg:grid max-lg:gap-4 max-sm:grid-cols-4 max-lg:grid-cols-3 max-lg:items-center"
-          >
-            <img
-              className="aspect-[3/2] object-cover mx-auto max-lg:col-span-1"
-              src={event.img}
-            />
-            <div className="space-y-0.5 max-sm:col-span-3 max-lg:col-span-2">
-              <h3 className="mb-1.5 heading-sm truncate">{event.title}</h3>
-              <div className="flex items-center gap-1">
-                <svg
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="-ml-1 text-neutral-500 shrink-0"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M18.25 11C18.25 15 12 19.25 12 19.25C12 19.25 5.75 15 5.75 11C5.75 7.5 8.68629 4.75 12 4.75C15.3137 4.75 18.25 7.5 18.25 11Z"
-                  ></path>
-                  <circle
-                    cx="12"
-                    cy="11"
-                    r="2.25"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                  ></circle>
-                </svg>
-                <span className="truncate">{event.address}</span>
-              </div>
-              <div className="flex items-center gap-1">
-                <svg
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="-ml-1 text-neutral-500 shrink-0"
-                >
-                  <circle
-                    cx="12"
-                    cy="12"
-                    r="7.25"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                  ></circle>
-                  <path
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    d="M12 8V12L14 14"
-                  ></path>
-                </svg>
-
-                <span>{event.startTime}</span>
-                <svg
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-6 h-6 -mx-1.5 text-neutral-500"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                    d="M10.75 8.75L14.25 12L10.75 15.25"
-                  ></path>
-                </svg>
-                <span>{event.endTime}</span>
-              </div>
-              <div className="flex items-center gap-1">
-                <svg
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="-ml-1 text-neutral-500 shrink-0"
-                >
-                  <circle
-                    cx="12"
-                    cy="12"
-                    r="7.25"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                  ></circle>
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M14.25 8.75H11.375C10.4775 8.75 9.75 9.47754 9.75 10.375V10.375C9.75 11.2725 10.4775 12 11.375 12H12.625C13.5225 12 14.25 12.7275 14.25 13.625V13.625C14.25 14.5225 13.5225 15.25 12.625 15.25H9.75"
-                  ></path>
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M12 7.75V8.25"
-                  ></path>
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M12 15.75V16.25"
-                  ></path>
-                </svg>
-                <span>{event.price}</span>
-              </div>
-            </div>
-          </Link>
+          <CalendarEvent
+            key={`${index}`}
+            activeDay={activeDay}
+            index={index}
+            event={{
+              img: event.img,
+              title: event.title,
+              link: event.link,
+              address: event.address,
+              startTime: event.startTime,
+              endTime: event.endTime,
+              price: event.price,
+            }}
+          />
         ))}
       </div>
       <div className="hidden w-full gap-3 lg:flex">
@@ -273,131 +167,22 @@ const CalendarWeek: React.FC = () => {
             <div className="hidden mb-2 lg:block">
               <span className="heading">{day}</span>
             </div>
-            <div className="flex flex-col gap-3">
+            <div className="grid grid-cols-1 gap-3 p-px">
               {events.map((event: Event, index: number) => (
-                <Link
-                  key={`${day}-event-${index}`}
-                  to={event.link}
-                  className="block p-3 text-sm card max-lg:grid max-lg:gap-4 max-sm:grid-cols-4 max-lg:grid-cols-3 max-lg:items-center"
-                >
-                  <img
-                    className="aspect-[3/2] object-cover mx-auto max-lg:col-span-1"
-                    src={event.img}
-                  />
-                  <div className="space-y-0.5 max-sm:col-span-3 max-lg:col-span-2">
-                    <h3 className="mb-1.5 heading-sm truncate">
-                      {event.title}
-                    </h3>
-                    <div className="flex items-center gap-1">
-                      <svg
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="-ml-1 text-neutral-500 shrink-0"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1.5"
-                          d="M18.25 11C18.25 15 12 19.25 12 19.25C12 19.25 5.75 15 5.75 11C5.75 7.5 8.68629 4.75 12 4.75C15.3137 4.75 18.25 7.5 18.25 11Z"
-                        ></path>
-                        <circle
-                          cx="12"
-                          cy="11"
-                          r="2.25"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1.5"
-                        ></circle>
-                      </svg>
-                      <span className="truncate">{event.address}</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <svg
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="-ml-1 text-neutral-500 shrink-0"
-                      >
-                        <circle
-                          cx="12"
-                          cy="12"
-                          r="7.25"
-                          stroke="currentColor"
-                          strokeWidth="1.5"
-                        ></circle>
-                        <path
-                          stroke="currentColor"
-                          strokeWidth="1.5"
-                          d="M12 8V12L14 14"
-                        ></path>
-                      </svg>
-
-                      <span>{event.startTime}</span>
-                      <svg
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="w-6 h-6 -mx-1.5 text-neutral-500"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1"
-                          d="M10.75 8.75L14.25 12L10.75 15.25"
-                        ></path>
-                      </svg>
-                      <span>{event.endTime}</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <svg
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="-ml-1 text-neutral-500 shrink-0"
-                      >
-                        <circle
-                          cx="12"
-                          cy="12"
-                          r="7.25"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1.5"
-                        ></circle>
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1.5"
-                          d="M14.25 8.75H11.375C10.4775 8.75 9.75 9.47754 9.75 10.375V10.375C9.75 11.2725 10.4775 12 11.375 12H12.625C13.5225 12 14.25 12.7275 14.25 13.625V13.625C14.25 14.5225 13.5225 15.25 12.625 15.25H9.75"
-                        ></path>
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1.5"
-                          d="M12 7.75V8.25"
-                        ></path>
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1.5"
-                          d="M12 15.75V16.25"
-                        ></path>
-                      </svg>
-                      <span>{event.price}</span>
-                    </div>
-                  </div>
-                </Link>
+                <CalendarEvent
+                  key={`${index}`}
+                  activeDay={activeDay}
+                  index={index}
+                  event={{
+                    img: event.img,
+                    title: event.title,
+                    link: event.link,
+                    address: event.address,
+                    startTime: event.startTime,
+                    endTime: event.endTime,
+                    price: event.price,
+                  }}
+                />
               ))}
             </div>
           </div>
