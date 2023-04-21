0dc99f55ceaf6ddbe9053b0f8cc8cc69467ff1a1
James <jamestoone@gmail.com>
2023-04-21 07:34:18 +0100

    fixes

--- src/components/checkout/checkout-utils.ts
+++ src/components/checkout/checkout-utils.ts
@@ -1,14 +1,13 @@
-export const calculateAge = (dob: Date, currentDate: Date = new Date()) => {
+export const calculateAge = (
+  dob: Date | string,
+  currentDate: Date = new Date()
+): number => {
   const birthDate = new Date(dob);
-  let age = currentDate.getFullYear() - birthDate.getFullYear();
-  const monthDifference = currentDate.getMonth() - birthDate.getMonth();
-
-  if (
-    monthDifference < 0 ||
-    (monthDifference === 0 && currentDate.getDate() < birthDate.getDate())
-  ) {
-    age--;
-  }
-
+  const yearsDifference = currentDate.getFullYear() - birthDate.getFullYear();
+  const isBirthdayNotReached =
+    currentDate.getMonth() < birthDate.getMonth() ||
+    (currentDate.getMonth() === birthDate.getMonth() &&
+      currentDate.getDate() < birthDate.getDate());
+  const age = isBirthdayNotReached ? yearsDifference - 1 : yearsDifference;
   return age;
 };
