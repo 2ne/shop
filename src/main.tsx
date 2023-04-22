66075939408127438e4a0c372982e3dbbb7f030b
James <jamestoone@gmail.com>
2023-04-22 18:04:05 +0100

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { ConfigProvider } from "antd";
 import "./App.css";
 import App from "./App";
 import { orgColours } from "./org";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: orgColours?.interactive ?? "#056eef",
-          colorPrimaryBg: "#f5f5f5", // tw neutral 100
-          colorTextHeading: "#262626", // tw neutral 800
-          colorText: "#262626", // tw neutral 800
-          colorTextDescription: "#737373", // tw neutral 500
-          colorSuccess: "#10b981", // tw emerald 500
-          colorWarning: "#f59e0b", // tw amber 500
-          colorError: "#f43f5e", // tw rose 500
-          colorInfo: orgColours?.interactive ?? "#056eef",
-          fontFamily:
-            "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif",
-          borderRadius: 6,
-          boxShadow:
-            "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-          boxShadowSecondary:
-            "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root") as HTMLElement;
+
+createRoot(rootElement).render(
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: orgColours?.interactive ?? "#056eef",
+        colorPrimaryBg: "#f5f5f5", // tw neutral 100
+        colorTextHeading: "#262626", // tw neutral 800
+        colorText: "#262626", // tw neutral 800
+        colorTextDescription: "#737373", // tw neutral 500
+        colorSuccess: "#10b981", // tw emerald 500
+        colorWarning: "#f59e0b", // tw amber 500
+        colorError: "#f43f5e", // tw rose 500
+        colorInfo: orgColours?.interactive ?? "#056eef",
+        fontFamily:
+          "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif",
+        borderRadius: 6,
+        boxShadow:
+          "0 1px 3px 0 rgb(0 0 0 / / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        boxShadowSecondary:
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+      },
+    }}
+  >
+    <App />
+  </ConfigProvider>
 );
