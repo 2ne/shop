4b3284dfdf23d8bd79726f93c681e7cad1463173
James <jamestoone@gmail.com>
2023-04-24 20:36:55 +0100

    Update checkout-03-medical-information.tsx

--- src/components/checkout/checkout-03-medical-information.tsx
+++ src/components/checkout/checkout-03-medical-information.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef, useImperativeHandle, useState } from "react";
 import { Collapse, Form, Input, Radio, RadioChangeEvent } from "antd";
 import CheckoutStepHeader from "./checkout-header";
 import { useBasketContext } from "../basket/basket-context";
-import { BasketItem, MedicalInfo, medicalInfoFields } from "../../types/types";
+import { MedicalInfo, Participant, medicalInfoFields } from "../../types/types";
 
 export interface CheckoutMedicalInfoHandles {
   submitForm: () => Promise<boolean>;
@@ -25,6 +25,25 @@ const CheckoutMedicalInfo = forwardRef<
     const { basketItems, addMedicalInfo } = useBasketContext();
     const [medicalInformationForm] = Form.useForm();
     const [medicalFields, setMedicalFields] = useState(false);
+
+    // Create a Set to store unique participant IDs
+    const participantIds = new Set<number>();
+
+    // Generate a unique participant list by filtering basket items' participants based on their unique IDs
+    const participants = basketItems
+      .flatMap((item) => {
+        // Get participants from item or use empty array if none
+        return item.participants ?? [];
+      })
+      .filter((participant) => {
+        // Keep participant in array if ID is not already in Set, then add ID to Set
+        if (!participantIds.has(participant.id)) {
+          participantIds.add(participant.id);
+          return true;
+        }
+        // Exclude participant if ID already in Set
+        return false;
+      });
 
     useImperativeHandle(ref, () => ({
       // The 'submitForm' function is exposed to the parent component (checkout) via the ref so it can be called externally to trigger form validation and submission
@@ -52,31 +71,25 @@ const CheckoutMedicalInfo = forwardRef<
     // This function is called when the form is submitted
     const onDetailsFinish = (
       values: { [key: string]: any },
-      items: BasketItem[]
+      participants: Participant[]
     ) => {
-      // Loop through each item in the items array
-      items.forEach((item) => {
-        // Get the participants array from the current item, defaulting to an empty array if undefined
-        const participants = item.participants ?? [];
+      // Loop through each participant in the participants array
+      participants.forEach((participant) => {
+        // Get the participant's ID
+        const participantId = participant.id;
 
-        // Loop through each participant in the participants array
-        participants.forEach((participant) => {
-          // Get the participant's ID
-          const participantId = participant.id;
+        // Create an object to store the participant's medical information with the correct type
+        const medicalInfo: MedicalInfo = {};
 
-          // Create an object to store the participant's medical information with the correct type
-          const medicalInfo: MedicalInfo = {};
-
-          // Loop through each field in the medicalInfoFields array
-          medicalInfoFields.forEach((field) => {
-            // Get the value for the current field from the form values object
-            medicalInfo[field.key as keyof MedicalInfo] =
-              values[`participant_${participantId}_${field.key}`];
-          });
-
-          // Call the addMedicalInfo function with the participant's ID and the medical info object
-          addMedicalInfo(participantId, medicalInfo);
+        // Loop through each field in the medicalInfoFields array
+        medicalInfoFields.forEach((field) => {
+          // Get the value for the current field from the form values object
+          medicalInfo[field.key as keyof MedicalInfo] =
+            values[`participant_${participantId}_${field.key}`];
         });
+
+        // Call the addMedicalInfo function with the participant's ID and the medical info object
+        addMedicalInfo(participantId, medicalInfo);
       });
 
       // Log the basketItems to the console. Don't show required products
@@ -124,103 +137,88 @@ const CheckoutMedicalInfo = forwardRef<
           layout="vertical"
           form={medicalInformationForm}
           name="medicalInformationForm"
-          onFinish={(values) => onDetailsFinish(values, basketItems)}
+          onFinish={(values) => onDetailsFinish(values, participants)}
           onFinishFailed={onDetailsFinishFailed}
           className="space-y-6 text-left hide-validation-asterix"
         >
-          {basketItems.map((item) => {
-            const participants = item.participants ?? [];
-            return (
-              <div key={item.id}>
-                {participants.map((participant, index) => {
-                  return (
-                    <div
-                      key={`participant_${index}`}
-                      className="p-3 border rounded-md border-neutral-200 [&:has(.ant-form-item-has-error)]:border-error"
-                    >
-                      <div className="mb-2 font-medium">
-                        {participant.firstName} {participant.lastName}
-                      </div>
-                      <Form.Item
-                        name={`participant_${participant.id}`}
-                        label="Any known disabilities, medical/behavioural conditions, dietary needs or current medications?"
-                        className="!mb-0.5 [&_.ant-form-item-label]:font-normal"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Please select an option",
-                          },
-                        ]}
-                      >
-                        <Radio.Group
-                          className="flex text-center [&>*]:flex-1 mt-1"
-                          onChange={(e) => onMedicalChange(e)}
-                        >
-                          <Radio.Button value="no">No</Radio.Button>
-                          <Radio.Button value="yes">Yes</Radio.Button>
-                        </Radio.Group>
-                      </Form.Item>
-                      {medicalFields && (
-                        <Collapse className="mt-4">
-                          <Collapse.Panel header="Doctor's details" key="1">
-                            {medicalInfoFields
-                              .filter(
-                                (field) => field.group === "doctorsDetails"
-                              )
-                              .map((field) => (
-                                <Form.Item
-                                  key={field.key}
-                                  name={`participant_${participant.id}_${field.key}`}
-                                  label={field.label}
-                                  className="last:!mb-1.5"
-                                >
-                                  <Input />
-                                </Form.Item>
-                              ))}
-                          </Collapse.Panel>
-                          <Collapse.Panel header="Medical information" key="2">
-                            {medicalInfoFields
-                              .filter(
-                                (field) => field.group === "medicalInformation"
-                              )
-                              .map((field) => (
-                                <Form.Item
-                                  key={field.key}
-                                  name={`participant_${participant.id}_${field.key}`}
-                                  label={field.label}
-                                  className="last:!mb-1.5"
-                                >
-                                  <Input />
-                                </Form.Item>
-                              ))}
-                          </Collapse.Panel>
-                          <Collapse.Panel
-                            header="Allergies & dietary requirements"
-                            key="3"
-                          >
-                            {medicalInfoFields
-                              .filter(
-                                (field) => field.group === "allergiesAndDietary"
-                              )
-                              .map((field) => (
-                                <Form.Item
-                                  key={field.key}
-                                  name={`participant_${participant.id}_${field.key}`}
-                                  label={field.label}
-                                  className="last:!mb-1.5"
-                                >
-                                  <Input />
-                                </Form.Item>
-                              ))}
-                          </Collapse.Panel>
-                        </Collapse>
-                      )}
-                    </div>
-                  );
-                })}
+          {participants.map((participant, index) => (
+            <div
+              key={`participant_${index}`}
+              className="p-3 border rounded-md border-neutral-200 [&:has(.ant-form-item-has-error)]:border-error"
+            >
+              <div className="mb-2 font-medium">
+                {participant.firstName} {participant.lastName}
               </div>
-            );
-          })}
+              <Form.Item
+                name={`participant_${participant.id}`}
+                label="Any known disabilities, medical/behavioural conditions, dietary needs or current medications?"
+                className="!mb-0.5 [&_.ant-form-item-label]:font-normal"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please select an option",
+                  },
+                ]}
+              >
+                <Radio.Group
+                  className="flex text-center [&>*]:flex-1 mt-1"
+                  onChange={(e) => onMedicalChange(e)}
+                >
+                  <Radio.Button value="no">No</Radio.Button>
+                  <Radio.Button value="yes">Yes</Radio.Button>
+                </Radio.Group>
+              </Form.Item>
+              {medicalFields && (
+                <Collapse className="mt-4">
+                  <Collapse.Panel header="Doctor's details" key="1">
+                    {medicalInfoFields
+                      .filter((field) => field.group === "doctorsDetails")
+                      .map((field) => (
+                        <Form.Item
+                          key={field.key}
+                          name={`participant_${participant.id}_${field.key}`}
+                          label={field.label}
+                          className="last:!mb-1.5"
+                        >
+                          <Input />
+                        </Form.Item>
+                      ))}
+                  </Collapse.Panel>
+                  <Collapse.Panel header="Medical information" key="2">
+                    {medicalInfoFields
+                      .filter((field) => field.group === "medicalInformation")
+                      .map((field) => (
+                        <Form.Item
+                          key={field.key}
+                          name={`participant_${participant.id}_${field.key}`}
+                          label={field.label}
+                          className="last:!mb-1.5"
+                        >
+                          <Input />
+                        </Form.Item>
+                      ))}
+                  </Collapse.Panel>
+                  <Collapse.Panel
+                    header="Allergies & dietary requirements"
+                    key="3"
+                  >
+                    {medicalInfoFields
+                      .filter((field) => field.group === "allergiesAndDietary")
+                      .map((field) => (
+                        <Form.Item
+                          key={field.key}
+                          name={`participant_${participant.id}_${field.key}`}
+                          label={field.label}
+                          className="last:!mb-1.5"
+                        >
+                          <Input />
+                        </Form.Item>
+                      ))}
+                  </Collapse.Panel>
+                </Collapse>
+              )}
+            </div>
+          ))}
         </Form>
       </>
     );
