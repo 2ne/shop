b456402ae764b1c3d88b57e07edd0f33fff344c8
James <jamestoone@gmail.com>
2023-05-02 21:37:04 +0100

    Update account-modal.tsx

--- src/components/account/account-modal.tsx
+++ src/components/account/account-modal.tsx
@@ -192,7 +192,7 @@ const AccountModal: React.FC<AccountModalProps> = ({
       <div className="md:grid md:grid-cols-[14rem,1fr] md:gap-x-6">
         <aside>
           <Menu
-            className="!border-0 [&>li]:!mx-0 [&>li]:!rounded-md [&>li]:!px-3s [&>li]:!flex [&>li]:!items-center"
+            className="!border-0 [&>li]:!mx-0 [&>li]:!rounded-md [&>li]:!px-3s [&>li]:!flex [&>li]:!items-center [&>li.ant-menu-item-selected]:bg-interactive/5 [&>li.ant-menu-item-selected]:font-medium"
             items={items}
             selectedKeys={[selectedMenuKey]}
             onSelect={handleMenuSelect}
